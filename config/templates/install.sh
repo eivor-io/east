d66a4743008aaacd102abc1da1613b58e65c65c4
Jay <jgodara@sarvika.com>
2019-06-11 12:00:05 +0000

    feat: Adding support for voidlinux

--- config/templates/install.sh
+++ config/templates/install.sh
@@ -5,7 +5,70 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
+## Functions
+
+function os_version() {
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+        OS=$ID
+        VER=$VERSION_ID
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+        VER=$DISTRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        OS=Debian
+        VER=$(cat /etc/debian_version)
+    elif [ -f /etc/SuSe-release ]; then
+        # Older SuSE/etc.
+        ...
+    elif [ -f /etc/redhat-release ]; then
+        # Older Red Hat, CentOS, etc.
+        ...
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        OS=$(uname -s)
+        VER=$(uname -r)
+    fi
+
+    echo "$OS"
+}
+
+function system_update_pacman() {
+    pacman -Syyu
+}
+
+function system_update_xbps() {
+    xbps-install -Su
+}
+
+function install_packages_pacman() {
+    pacman -Sy $@
+}
+
+function install_packages_xbps() {
+    xbps-install -S $@
+}
+
+export OS=$(os_version)
+export PACKAGECTL=""
+case $OS in
+    "arch" | "manjaro") PACKAGECTL="pacman" ;;
+    "void") PACKAGECTL="xbps" ;;
+    *) 
+        echo "Only Arch and Voidlinux are supported"
+        echo "Visit https://eivor.xyz/east to request support" ;;
+esac
+
 echo "Running as user: $USER."
+echo "Detected OS: $OS"
 read -p "Perform installation? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -21,7 +84,7 @@ declare -a packages=({% for package in packages %}
                     )
 
 echo "Performing a system udpate."
-sudo pacman -Syyu
+sudo bash -c "$(declare -f system_update_${PACKAGECTL}); system_update_${PACKAGECTL}"
 
 {% if presync|length > 0 %}
 echo "Executing {{presync|length}} pre-sync scripts..."
@@ -40,7 +103,7 @@ fi
 {% endif %}
 
 echo "Installing packages..."
-sudo pacman -Sy ${packages[@]}
+sudo bash -c "$(declare -f install_packages_${PACKAGECTL}); install_packages_${PACKAGECTL} ${packages[@]}"
 
 {% if postsync|length > 0 %}
 echo "Executing {{postsync|length}} post-sync scripts..."
